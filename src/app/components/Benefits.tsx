4771b9e29e9e45b0945bbd5e2fcfd08b6794633f
AllyssonCidade <allyssoncidade@gmail.com>
2024-12-14 14:47:02 -0300

    feat: Criar Benefits responsivo

--- src/app/components/Benefits.tsx
+++ src/app/components/Benefits.tsx
@@ -3,20 +3,20 @@ import React from "react";
 
 const Benefits = () => {
   return (
-    <section className="flex flex-row justify-between px-24">
-      <div className="flex py-6 flex-col gap-10">
+    <section className="flex flex-row justify-between px-2 py-3 md:py-4 lg:py-8 xl:py-12 md:px-3 lg:px-14 xl:px-24 ">
+      <div className="flex gap-2 md:gap-6 flex-col lg:gap-10">
         <div>
-          <h3 className="text-3xl w-96">
+          <h3 className="text-2xl lg:text-3xl lg:w-96">
             Seu progresso com artigos e vídeos de qualidade
           </h3>
-          <p className="text-sm w-96">
+          <p className="lg:text-sm lg:w-96 text-xs">
             Informação útil para seus estudos, sendo entregue de forma singular
             e agradável
           </p>
         </div>
         <div>
           <div className="flex flex-row">
-            <div className="w-10 h-10 relative">
+            <div className="lg:w-10 lg:h-10 relative w-8 h-8">
               <Image
                 src="/images/Arrow.png"
                 alt="Seta diagonal esquerda/direita para cima"
@@ -24,13 +24,17 @@ const Benefits = () => {
               />
             </div>
             <div>
-              <h4 className="text-2xl w-72">66% a mais de chance</h4>
-              <p className="text-sm w-72">de passar em um concurso público</p>
+              <h4 className="text-lg lg:text-2xl lg:w-72 ">
+                66% a mais de chance
+              </h4>
+              <p className="lg:text-sm lg:w-72 text-xs">
+                de passar em um concurso público
+              </p>
             </div>
           </div>
         </div>
       </div>
-      <div className="w-72 h-64 relative mr-32">
+      <div className="hidden md:block md:w-52 lg:w-72 md:h-48 lg:h-64 relative md:mr-5 lg:mr-10 xl:mr-32">
         <Image src="/images/Benefits.png" alt="Motivos de sucesso" fill />
       </div>
     </section>
