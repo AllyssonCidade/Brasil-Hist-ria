dcb81ed828f7a2253ea0cd846b67eb278e44894c
AllyssonCidade <allyssoncidade@gmail.com>
2024-12-15 18:12:27 -0300

    feat: Add Articles in home

---
+++ src/app/components/Articles.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import ArticlesArray from "../_data/articleMockup.json";
+import Image from "next/image";
+
+export interface ArticleType {
+  id: number;
+  capa: string;
+  data: string;
+  autor: string;
+  titulo: string;
+  texto: string;
+}
+const articles: ArticleType[] = ArticlesArray;
+
+function Articles() {
+  return (
+    <div className="flex flex-col gap-10 px-4 mt-4">
+      <div className="w-full h-px bg-black" />
+      <h3 className="text-center text-3xl tracking-25p -mt-4">Artigos</h3>
+      <div className="grid gap-10 justify-center grid-cols-1 md:grid-cols-2 md:px-16 xl:px-48 ">
+        {articles.map((article) => (
+          <div key={article.id} className="w-full justify-between">
+            <div className="w-12/12 mx-auto h-64 relative">
+              <Image src={article.capa} alt={article.titulo} fill />
+            </div>
+            <p className="text-xs mt-2">
+              {article.data} | Por: {article.autor}
+            </p>
+            <h3 className="text-xl font-medium">{article.titulo}</h3>
+          </div>
+        ))}
+        {articles.map((article) => (
+          <div key={article.id} className="w-full justify-between">
+            <div className="w-12/12 mx-auto h-64 relative">
+              <Image src={article.capa} alt={article.titulo} fill />
+            </div>
+            <p className="text-xs mt-2">
+              {article.data} | Por: {article.autor}
+            </p>
+            <h3 className="text-xl font-medium">{article.titulo}</h3>
+          </div>
+        ))}
+      </div>
+      <button className="lg:text-2xl mt-4 self-center font-medium bg-neutral-300 p-1 px-4">
+        Ver todos
+      </button>
+    </div>
+  );
+}
+
+export default Articles;
