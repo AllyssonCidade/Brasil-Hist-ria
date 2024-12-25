2327fdbcdd0e8b0d2df9d14da4e29bc6a21fc14e
AllyssonCidade <allyssoncidade@gmail.com>
2024-12-25 00:10:21 -0300

    hotFix: remove banner from useEffect in banner component

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -6,7 +6,7 @@ import React, { useEffect } from "react";
 // USAR HOST EM AMBIENTE DE DESENVOLVIMENTO
 //não sei por que está fazendo isso, mas funciona(antes de subir remova a variavel host )
 //em desenvolvimento use uma variavel HOST pegando o .env do localhost e coloque antes das imagens
-// const HOST = process.env.NEXT_PUBLIC_HOST_IMG_API;
+const HOST = process.env.NEXT_PUBLIC_HOST_IMG_API;
 
 const Banner = () => {
   const [banner, setBanner] = React.useState<BannerProps>();
@@ -27,7 +27,7 @@ const Banner = () => {
   return (
     <section className="w-full h-60 sm:h-80 md:h-80 lg:h-96 xl:h-lg440 2xl:h-screen relative">
       <img
-        src={`${banner?.banner[0].url}`}
+        src={`${HOST}${banner?.banner[0].url}`}
         className="object-cover w-full h-full"
         alt="Banner"
       />
